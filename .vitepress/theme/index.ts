88816de62ee67a0eb219b0abc80eb663da6d75bb
LittleChest <81231195+LittleChest@users.noreply.github.com>
2024-02-13 13:55:58 +0800

    :lipstick: Add NProgress

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,4 +1,11 @@
 import DefaultTheme from 'vitepress/theme'
+import vitepressNprogress from 'vitepress-plugin-nprogress'
+import 'vitepress-plugin-nprogress/lib/css/index.css'
 import './index.css'
 
-export default DefaultTheme
+export default {
+  ...DefaultTheme,
+  enhanceApp: (ctx) => {
+    vitepressNprogress(ctx)
+  },
+}
