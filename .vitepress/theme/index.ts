d126f6cfedf95f95a312418f8185e510e131a75e
LittleChest <81231195+LittleChest@users.noreply.github.com>
2024-03-13 16:51:02 +0800

    ✨ Add back to top button

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,16 @@
 import DefaultTheme from 'vitepress/theme'
 import vitepressNprogress from 'vitepress-plugin-nprogress'
 import 'vitepress-plugin-nprogress/lib/css/index.css'
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
 import './index.css'
 
 export default {
   ...DefaultTheme,
   enhanceApp: (ctx) => {
     vitepressNprogress(ctx)
+    vitepressBackToTop({
+      threshold: 300,
+    })
   },
 }
