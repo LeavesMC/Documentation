148f402bdc6ac587f52590cc4b8e0e539a1adb5f
LittleChest <81231195+LittleChest@users.noreply.github.com>
2024-04-04 00:20:52 +0800

    :money_with_wings: This site is powered by Netlify

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,17 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #aside-ads-before>
+      <div class="NetfilyAds">
+        <a href="https://www.netlify.com/">
+          <img src="/Netfily.svg" alt="This site is powered by Netlify" />
+        </a>
+      </div>
+    </template>
+  </Layout>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,10 +2,12 @@ import DefaultTheme from 'vitepress/theme'
 import vitepressNprogress from 'vitepress-plugin-nprogress'
 import 'vitepress-plugin-nprogress/lib/css/index.css'
 import './index.css'
+import Layout from './Layout.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp: (ctx) => {
     vitepressNprogress(ctx)
   },
+  Layout: Layout,
 }
