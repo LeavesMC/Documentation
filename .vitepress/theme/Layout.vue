c490b847a06dea5aeb1a67ecfb794487f8b4f656
LittleChest <81231195+LittleChest@users.noreply.github.com>
2024-04-11 23:43:20 +0800

    :sparkles: Language redirects

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,7 +1,17 @@
-<script setup>
+<script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
+import { useData } from 'vitepress'
+import { watchEffect } from 'vue'
+import { inBrowser } from 'vitepress'
 
 const { Layout } = DefaultTheme
+const { lang } = useData()
+
+watchEffect(() => {
+  if (inBrowser) {
+    document.cookie = `nf_lang=${lang.value}; expires=Mon, 1 Jan 2030 00:00:00 UTC; path=/`
+  }
+})
 </script>
 
 <template>
